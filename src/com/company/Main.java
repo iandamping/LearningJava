4535e2e26c76651b9d2163dc27bfc4b3c05f2611
ian damping <iandamping92@gmail.com>
2021-02-28 13:49:23 +0700

    new repo for learning basic Java

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import com.company.books.Book;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // write your code here
+        final Book myBook = new Book();
+        myBook.setTitle("Harry Potter");
+        System.out.println(myBook.getTitle());
+
+    }
+}
---
+++ src/com/company/books/Book.java
@@ -0,0 +1,21 @@
+package com.company.books;
+
+public class Book {
+
+    // Fields / Atributes / Variable
+    String title;
+    String author;
+    int numberOfPages;
+
+    // Method / Function
+    public void setTitle(String bookTittle){
+        title = bookTittle;
+    }
+
+    // Method / Function
+    public String getTitle() {
+        return title;
+    }
+
+
+}
